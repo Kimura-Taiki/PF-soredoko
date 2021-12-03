503193322c051f1591865ca20ced0e4d1a65afec
“Kimura-Taiki” <taiguimucun850@gmail.com>
2021-12-03 08:46:01 +0000

    文字サイズを修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
   root to: 'homes#top'
   get 'homes/about', as: 'about'
+  resource :user
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # ルーティングに駄文を入れる
 end
